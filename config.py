ebf7f9af7476986e53ae2e0dcdff5d0baafcc19b
Safura <safurabalqiss@gmail.com>
2025-08-02 01:42:10 +0800

    feat(config): add support for remote database connection
    Add environment variable USE_REMOTE_DB to switch between local and remote database connections. This provides flexibility for different deployment environments.

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/dashboard_db'
+import os
+
+USE_REMOTE_DB = os.getenv("USE_REMOTE_DB", "false").lower() == "true"
+
+if USE_REMOTE_DB:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql12793151:VWpIPv78A8@sql12.freesqldatabase.com:3306/sql12793151'
+else:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/dashboard_db'
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
