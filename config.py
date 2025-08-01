4c1859e045d2812d45e327ee6d287407bedbd85d
Safura <safurabalqiss@gmail.com>
2025-08-02 02:18:06 +0800

    fix(config): replace hardcoded remote db uri with env variable
    Use environment variable for remote database URI to improve security and configuration flexibility

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 USE_REMOTE_DB = os.getenv("USE_REMOTE_DB", "false").lower() == "true"
 
 if USE_REMOTE_DB:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql12793151:VWpIPv78A8@sql12.freesqldatabase.com:3306/sql12793151'
+    SQLALCHEMY_DATABASE_URI = os.getenv("REMOTE_DB_URI")
 else:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/dashboard_db'
 
